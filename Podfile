3c4ad3f027149d7bf07e70bcc04d4d8404f62716
Shayan Mehranpoor <shayanmehranpoor@yahoo.com>
2020-06-18 10:13:51 +0430

    pod added

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '12.2'
+
+target 'Communere' do
+  use_frameworks!
+
+  pod 'RxSwift'
+  pod 'RxCocoa'
+
+end
---
+++ Pods/Target Support Files/Pods-Communere/Pods-Communere-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Communere : NSObject
+@end
+@implementation PodsDummy_Pods_Communere
+@end
